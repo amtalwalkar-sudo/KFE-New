fdeb3df2db9168cb19f5d2a66dd388441dc87608
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-09-16 08:40:53 +0530

    Delete obsolete shell service after startup extraction

--- src/application/shell/shellService.js
+++
@@ -1,8 +0,0 @@
-import { initializeCanonicalStorage } from '../../utils/indexedDB.js'
-
-export const ShellService = Object.freeze({
-  async initialize() {
-    await initializeCanonicalStorage()
-    return { ready: true }
-  },
-})
