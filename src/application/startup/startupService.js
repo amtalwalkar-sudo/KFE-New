3e21b2b252f1b799a6fd27fa61092db42e45f42f
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-09-16 08:40:37 +0530

    Centralize application startup orchestration

--- src/application/startup/startupService.js
+++ src/application/startup/startupService.js
@@ -1,7 +1,22 @@
 import { MutationRepository } from '../../repositories/mutationRepository.js'
+import { BackupService } from '../backup/backupService.js'
+import { CloudBackupLifecycle } from '../backup/cloudBackupLifecycle.js'
 
-export const StartupService = Object.freeze({
-  async recoverPendingMutations() {
-    return MutationRepository.recoverStaleSyncing()
-  },
-})
+let platform = null
+export const configureStartupPlatform = adapter => {
+  if (!adapter || typeof adapter.initializeStorage !== 'function' || typeof adapter.registerServiceWorker !== 'function') throw new TypeError('Invalid startup platform adapter.')
+  platform = adapter
+}
+
+export const initializeApplication = async () => {
+  if (!platform) throw new Error('Startup platform adapter has not been configured.')
+  await platform.initializeStorage()
+  await platform.registerServiceWorker()
+  await BackupService.maybeDailyLocalBackup()
+  await CloudBackupLifecycle.registerDailyCloudBackupSchedule()
+  await CloudBackupLifecycle.maybeDailyCloudBackup()
+  await MutationRepository.recoverStaleSyncing()
+  return { ready: true }
+}
+
+export const StartupService = Object.freeze({ configureStartupPlatform, initializeApplication, recoverPendingMutations: () => MutationRepository.recoverStaleSyncing() })
