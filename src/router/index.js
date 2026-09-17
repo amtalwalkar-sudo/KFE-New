6a1d7d845c850c670ff8d45421b3bc877fb78c3d
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-09-17 18:01:27 +0530

    feat: add timeline route

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import WorkModuleView from '../views/WorkModuleView.vue'
 import PerformanceView from '../views/PerformanceView.vue'
 import AdminView from '../views/AdminView.vue'
+import TimelineView from '../views/TimelineView.vue'
 
 const routes = [
   { path: '/', name: 'Work', component: WorkModuleView },
+  { path: '/timeline', name: 'Timeline', component: TimelineView },
   { path: '/performance', name: 'Performance', component: PerformanceView },
   { path: '/admin', name: 'Admin', component: AdminView },
 ]
