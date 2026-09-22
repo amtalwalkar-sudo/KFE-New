ffbad3181dd4bb093a71d733ee94252ce99513fc
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-09-22 20:30:31 +0530

    Replace broken Performance swipe navigation

--- src/views/PerformanceView.vue
+++ src/views/PerformanceView.vue
@@ -286,8 +286,6 @@ onBeforeUnmount(()=>unsubscribeChanges())
         </section>
       </template>
     </template>
-
-    <div class="performance-nav-handle" aria-label="Show navigation" role="button" tabindex="0" @click="window.dispatchEvent(new CustomEvent('kfe:performance-nav'))"><span>⌃</span></div>
   </section>
 </template>
 
