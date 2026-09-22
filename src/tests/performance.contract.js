fd207c7a444e98594edf9c109735b350fd312834
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-09-23 00:26:26 +0530

    Fix compliance negative balance contract expectation

--- src/tests/performance.contract.js
+++ src/tests/performance.contract.js
@@ -184,7 +184,7 @@ const provisionMetrics = derivePerformance(provisionSnapshot, range, previousRan
 near(provisionMetrics.maintenanceProvision, 600, 'maintenance provision must use applicable KM x rate')
 near(provisionMetrics.maintenanceProvisionBalance, -100, 'maintenance provision pool must allow negative balances')
 near(provisionMetrics.complianceProvisionById.c1, 24000 / 365, 'compliance provision must accrue per revenue day')
-near(provisionMetrics.complianceProvisionBalancesById.c1, -1000, 'compliance provision bucket must allow negative balances')
+near(provisionMetrics.complianceProvisionBalancesById.c1, (24000 / 365) - 25000, 'compliance provision bucket must allow negative balances')
 
 const historicalRateSnapshot = {
   ...engineSnapshot,
