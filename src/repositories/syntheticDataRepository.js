cadcc334c214a2a15717d46a27811d3d67484326
amtalwalkar-sudo <amtalwalkar@gmail.com>
2026-09-23 11:55:12 +0530

    fix: include synthetic settlements in isolated lifecycle

--- src/repositories/syntheticDataRepository.js
+++ src/repositories/syntheticDataRepository.js
@@ -1,6 +1,6 @@
 import { initializeSyntheticStorage, setActiveDataSource, getActiveDataSource, SYNTHETIC_DB_NAME } from '../utils/indexedDB.js'
 
-const SYNTHETIC_STORES = Object.freeze(['shifts','fuel_logs','odoGaps','pending_mutations','days','trips','gps_snapshots','movement_artifacts','vehicles','drivers','compliance_records','maintenance_records','loans','loan_payments','prepayments','driver_targets','break_even_inputs','settings','audit_history'])
+const SYNTHETIC_STORES = Object.freeze(['settlements','shifts','fuel_logs','odoGaps','pending_mutations','days','trips','gps_snapshots','movement_artifacts','vehicles','drivers','compliance_records','maintenance_records','loans','loan_payments','prepayments','driver_targets','break_even_inputs','settings','audit_history'])
 
 export const SyntheticDataRepository = Object.freeze({
   async writeSnapshot(snapshot) {
